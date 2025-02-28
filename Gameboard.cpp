38f68123e41d780e76115db23e7e2c30f524a18a
Martin Nguyen <martinnnguyen2732@gmail.com>
2025-02-27 23:34:39 -0500

    Figured out some logic and changed approach to placeMines()
    Beginning the implementation of a dictionary structure to hold spaces with a bomb instead of having to iterate through the whole 2-D array.

---
+++ Gameboard.cpp
@@ -0,0 +1,5 @@
+#include "Gameboard.h"
+
+Gameboard::Gameboard() {
+    board.resize
+}
--- Gameboard.h
+++ Gameboard.h
@@ -1,3 +1,32 @@
 #ifndef GAMEBOARD_H
 #define GAMEBOARD_H
 
+#include "Space.h"
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <cstdlib>
+#include <ctime>
+
+class GameBoard {
+    private:
+        static constexpr int WIDTH = 30;
+        static constexpr int HEIGHT = 16;
+        static constexpr int MINES = 99;
+
+        std::vector<std::vector<Space>> board;
+        std::unordered_map<int, std::unordered_set<int>> mineMap; // Using a dictionary-like implementation to store bomb locations.
+
+
+        void placeMines();
+        void calculateAdjacency();
+        int countAdjacentMines(int row, int col);
+
+    public:
+        GameBoard();
+        void resetBoard();
+        bool isMine(int row, int col);
+        Space& getSpace(int row, int col);
+};
+#endif
