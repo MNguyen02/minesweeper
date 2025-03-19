06d000e5ec31f1b5b9584b8e8fffdf7456868e4c
Martin Nguyen <martinnnguyen2732@gmail.com>
2025-03-19 16:58:14 -0400

    Starting placeMines() implementation.
    Began to implement placeMines() using a dictionary-like implementation.

--- Gameboard.cpp
+++ Gameboard.cpp
@@ -1,5 +1,23 @@
-#include "Gameboard.h"
+#include "GameBoard.h"
 
-Gameboard::Gameboard() {
-    board.resize
+GameBoard::GameBoard() {
+    board.resize(HEIGHT, std::vector<Space>(WIDTH));
+    resetBoard;
+}
+
+void GameBoard::resetBoard() {
+    placeMines();
+    calculateAdjacency();
+}
+
+void GameBoard::placeMines() {
+    int minesPlaced = 0;
+    while minesPlaced < 99 {
+        int row = std::rand() % HEIGHT;
+        int col = std::rand() % WIDTH;
+
+        if(mineMap.find(row) != mineMap.end && mineMap[row].find(col) != mineMap.end) {
+            mineMap.insert(row)
+        }
+    }
 }
